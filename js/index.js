7d997391ade4cdb25c7afeab8e0c41f12743f460
DenysDevelopment <dinis_05@ukr.net>
2021-03-16 16:31:29 +0200

    трохі доробив переключатель і добавив коментарі в js файл

--- js/index.js
+++ js/index.js
@@ -1,27 +1,32 @@
 "user strict";
 
+//получаєм переключатель теми
 const switchThemeElem = document.querySelector(".switch");
 
-function setLocalStorage(key, value) {
-  localStorage.setItem(key, value);
+//установлення даних локально в браузер
+function setLocalStorage(key, data) {
+  localStorage.setItem(key, data);
 }
 
+//отримання данных з локального сховища браузера
 function getLocalSrorage(key) {
   return localStorage.getItem(key);
 }
 
+//видалення даних з локального сховища
 function removeLocalSrorageItem(key) {
   localStorage.removeItem(key);
 }
 
+//установлення теми після загрузкі сторінкі
 function setLocalTheme() {
   if (JSON.parse(getLocalSrorage("theme"))) {
     document.body.classList.add("theme-toggle");
   }
 }
-
 setLocalTheme();
 
+//переключатель тем
 switchThemeElem.addEventListener("change", () => {
   if (document.body.classList.contains("theme-toggle")) {
     document.body.classList.remove("theme-toggle");
